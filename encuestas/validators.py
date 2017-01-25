cf7dc89c20e4737ed38ff6692d23cc3a29719a45
nicolas <sparkscs@gmail.com>
2017-01-25 15:03:38 -0300

    hotfix

--- encuestas/validators.py
+++ encuestas/validators.py
@@ -24,7 +24,7 @@ def verificar_rut(rut):
         digito = 10
     print digito_ingresado
     print digito
-    if digito != digito_ingresado:
+    if int(digito) != int(digito_ingresado):
         print "digito " + str(digito) + " ingresasado " + str(digito_ingresado)
         print "why :c"
         raise ValidationError(
