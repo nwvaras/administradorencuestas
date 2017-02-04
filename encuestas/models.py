27046b9a4a0942b593cb4f31d9606929bd1d612b
nicolas <sparkscs@gmail.com>
2017-02-04 20:45:34 -0300

    Hotfix

--- encuestas/models.py
+++ encuestas/models.py
@@ -38,9 +38,9 @@ class ConjuntosToSend(models.Model):
 
 class Request(models.Model):
     type = models.IntegerField()
-    title = models.CharField()
+    title = models.CharField(max_length=20)
     body = models.TextField()
-    user = models.CharField()
+    user = models.CharField(max_length=20)
 
 
     def to_dict(self):
