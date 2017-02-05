ca2390f4f6c87e74d865c2bf40ff08c7961cdbb3
nicolas <sparkscs@gmail.com>
2017-02-05 16:39:35 -0300

    fix

--- static/js/angular/controller_subject.js
+++ static/js/angular/controller_subject.js
@@ -135,6 +135,26 @@ $scope.selected = [];
 
     });
   }
+  $scope.toastPosition = angular.extend({},last);
+
+  $scope.getToastPosition = function() {
+    sanitizePosition();
+
+    return Object.keys($scope.toastPosition)
+      .filter(function(pos) { return $scope.toastPosition[pos]; })
+      .join(' ');
+  };
+
+  function sanitizePosition() {
+    var current = $scope.toastPosition;
+
+    if ( current.bottom && last.top ) current.top = false;
+    if ( current.top && last.bottom ) current.bottom = false;
+    if ( current.right && last.left ) current.left = false;
+    if ( current.left && last.right ) current.right = false;
+
+    last = angular.extend({},current);
+  }
   var afterChange= function (){
     console.log("yeah")
       cargarEncuestas()
