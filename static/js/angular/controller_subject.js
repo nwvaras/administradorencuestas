b24572284433573223f3d3d6b4bb4df751e05f1b
nicolas <sparkscs@gmail.com>
2017-02-05 17:08:20 -0300

    test

--- static/js/angular/controller_subject.js
+++ static/js/angular/controller_subject.js
@@ -223,7 +223,7 @@ $scope.selected = [];
     $scope.showDialogMessage= function(ev) {
     $mdDialog.show({
       controller: DialogConjunto,
-      templateUrl: '/static/html/angular/cargar_conjunto_final2.html',
+      templateUrl: '/static/html/angular/cargar_conjunto_final3.html',
       parent: angular.element(document.body),
       targetEvent: ev,
       clickOutsideToClose:true,
