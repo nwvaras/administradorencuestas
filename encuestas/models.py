ea912b053b3c31949d188e508397459db4c10427
Nicolás Varas <nwvaras@gmail.com>
2017-12-19 15:18:08 -0300

    Added date_responded field in json survey response

--- encuestas/models.py
+++ encuestas/models.py
@@ -288,6 +288,7 @@ class SendedSurvey(models.Model):
             'pk': self.pk,
             'respondida': self.respondida,
             'horaEnvio': self.date_creation,
+            'date_responded': self.date_responded,
             'survey': {
                 'titulo': self.survey.title,
                 'url': self.survey.url,
