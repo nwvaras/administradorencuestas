ec54d5f4801c88c852a904e6c9548b6e46314b85
nwvaras <nwvaras@gmail.com>
2018-03-10 16:33:26 -0300

    added sql info

--- administrador_encuestas/settings.py
+++ administrador_encuestas/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'administrador_encuestas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'quanto',
+        'USER': 'Jack',
+        'PASSWORD': 'Bennett',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
